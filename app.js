79aa22cc27b7b03240c8e992073def8f246f3c3f
matias300nes <matiendres@gmail.com>
2022-06-27 19:13:23 -0300

    eliminar tarea y temas oscuro-claro

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-console.log("Hola mundo")
-
-
 input = document.getElementById("tarea-input")
 btnAgregar = document.getElementById("btn-agregar")
 tareas = document.getElementById("tareas")
+chkTema = document.getElementById("chkTema")
 
 btnAgregar.addEventListener('click', (e)=>{
     //esto se ejecuta cuando se haga click en el boton
@@ -15,11 +13,27 @@ btnAgregar.addEventListener('click', (e)=>{
     nuevaTarea.innerHTML = `
         <input class="checkbox" type="checkbox">
         <p class="cuerpo-tarea">${input.value}</p>
-        <button class="btn-eliminar">
+        <button onClick="this.parentNode.remove()" class="btn-eliminar">
             <img src="./static/basura.svg" alt="Eliminar">
         </button>
     `
 
     tareas.appendChild(nuevaTarea)
+})
 
+chkTema.addEventListener('click', (e)=>{
+    root = document.querySelector(":root")
+    if(chkTema.checked){
+        root.style.setProperty("--color1", "#3f3f3f")
+        root.style.setProperty("--color2", "#fff")
+        root.style.setProperty("--color3", "#262626")
+        root.style.setProperty("--color4", "#EAEAEA")
+        root.style.setProperty("--color5", "#9C9C9C")
+    }else{
+        root.style.setProperty("--color1", "#fff")
+        root.style.setProperty("--color2", "#000")
+        root.style.setProperty("--color3", "#EAEAEA")
+        root.style.setProperty("--color4", "#3f3f3f")
+        root.style.setProperty("--color5", "#9C9C9C")
+    }
 })
